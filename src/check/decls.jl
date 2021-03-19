0361484f4f31bee5b2fbfe5f5234947494c2b4d0
quantapix <quantapix@gmail.com>
2021-03-19 10:09:20 -0400

    qpx

--- src/check/decls.jl
+++ src/check/decls.jl
@@ -1,4 +1,25 @@
+using JSON
 
+function create_tydecls(decls_fname :: String)
+  tds_json = JSON.parsefile(decls_fname)
+  tds = TyDecl[]
+  for td_json in tds_json
+    try
+      td = lj_parse_tydecl_json(td_json)
+      push!(tds, td)
+    catch e
+      #if Core.isdefined(:f_debug) && f_debug
+        #println(e)
+      #end
+    end
+  end
+  tds
+end
+
+make_tydecl_dict(tds) = 
+    Dict(zip( map(td-> "$(td.qual)::$(td.name)", tds)
+            , tds))
+            
 # Conditional includes to allow for standalone usage
 if !Core.isdefined(:LJ_SRC_FILE_ERRORS) 
   include("errors.jl")
