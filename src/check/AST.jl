b71b1f95d247dbf06255271dbe232523f94115a3
quantapix <quantapix@gmail.com>
2021-03-22 13:41:35 -0400

    qpx

--- src/check/AST.jl
+++ src/check/AST.jl
@@ -1,6 +1,5 @@
-# convenient type aliases
-const TVarSym = Symbol # type parameters
-const TNameSym = Symbol # names of types
+const TVarSym = Symbol
+const TNameSym = Symbol
 const TAttrSym = Symbol
 
 abstract type ASTBase end
@@ -8,7 +7,6 @@ abstract type ASTBase end
 struct TAny <: ASTBase end
 
 struct TUnion <: ASTBase
-    # ts is always normalised eg simplify_union(ts) = ts
     ts::Vector{ASTBase}
 end
 
@@ -28,7 +26,7 @@ struct TWhere <: ASTBase
     tvar::TVar
     lb::ASTBase # lower bound
     ub::ASTBase # upper bound
-    diag::Bool    # type var is diagonal # ROSSEXP
+    diag::Bool
 end
 
 TWhere(t,ts) = TWhere(t, ts, EmptyUnion, TAny(), false) # ROSSEXP
@@ -38,23 +36,30 @@ TWhere(t,ts,lb,ub) = TWhere(t, ts, lb, ub, false)
 struct TTuple <: ASTBase
     ts::Vector{ASTBase}
 end
+
 struct TName <: ASTBase
     name::TNameSym
     qual::String
 end
+
 TName(name::Symbol) = TName(name, "")
 TName(name::String) = TName(Symbol(name))
 TName(name::String, qual::String) = TName(Symbol(name), qual)
 
 struct TDataType <: ASTBase end
+
 struct TUnionAll <: ASTBase
     t::ASTBase
 end
+
 struct TSuperUnion <: ASTBase end
+
 struct TType <: ASTBase
     t::ASTBase
 end
+
 struct TSuperTuple <: ASTBase end
+
 struct TValue <: ASTBase
     v::String
 end
@@ -79,17 +84,9 @@ const TyDeclCol = Dict{String,TyDecl}
 
 import Base.==
 
-function ==(t1::TUnion, t2::TUnion)
-    t1.ts == t2.ts
-end
-
-function ==(t1::TApp, t2::TApp)
-    t1.t == t2.t && t1.ts == t2.ts
-end
-
-function ==(t1::TVar, t2::TVar)
-    t1.sym == t2.sym
-end
+==(t1::TUnion, t2::TUnion) = t1.ts == t2.ts
+==(t1::TApp, t2::TApp) = t1.t == t2.t && t1.ts == t2.ts
+==(t1::TVar, t2::TVar) = t1.sym == t2.sym
 
 function ==(t1::TWhere, t2::TWhere)
     t1.t == t2.t && t1.tvar == t2.tvar &&
@@ -97,67 +94,26 @@ function ==(t1::TWhere, t2::TWhere)
     t1.diag == t2.diag # ROSSEXP
 end
 
-function ==(t1::TTuple, t2::TTuple)
-    t1.ts == t2.ts
-end
+==(t1::TTuple, t2::TTuple) = t1.ts == t2.ts
 
-function skip_pre_dot(s::String)
-    if contains(s, ".")
-        return (true, s[searchindex(s, ".") + 1:end])
-    else
-        return (false, "")
-    end
-end
+skip_pre_dot(s::String) = contains(s, ".") ? (true, s[searchindex(s, ".") + 1:end]) : (false, "")
 
 function search_left_dot(t1::TName, t2::TName)
     (f, s1) = skip_pre_dot(string(t1.name))
-    if f
-        return Symbol(s1) == t2.name || search_left_dot(TName(Symbol(s1)), t2)
-    else
-        return false
-    end
+    f ? Symbol(s1) == t2.name || search_left_dot(TName(Symbol(s1)), t2) : false
 end
 
 function search_right_dot(t1::TName, t2::TName)
     (f, s2) = skip_pre_dot(string(t2.name))
-    if f
-        return t1.name == Symbol(s2) || search_right_dot(t1, TName(Symbol(s2)))
-    else
-        return false
-    end
+    f ? t1.name == Symbol(s2) || search_right_dot(t1, TName(Symbol(s2))) : false
 end
 
-function ==(t1::TName, t2::TName)
-#=  if t1.name == t2.name
-    return true
-  else
-    return search_left_dot(t1,t2) || search_right_dot(t1,t2)
-  end =#
-  # Base.Int should be == Int, but Profile.Tree.Node != Tree.Node
-    t1.name == t2.name && (t1.qual == t2.qual || t1.qual == "" || t2.qual == "")
-  # && (endswith(t1.qual, t2.qual) || endswith(t2.qual, t1.qual))
-end
-
-function ==(t1::TUnionAll, t2::TUnionAll)
-  # t1.t == t2.t
-    true
-end
-
-function ==(t1::TType, t2::TType)
-    t1.t == t2.t
-end
-
-function ==(t1::TSuperTuple, t2::TSuperTuple)
-    true
-end
-
-function ==(t1::TValue, t2::TValue)
-    t1.v == t2.v
-end
-
-function ==(t1::TyDecl, t2::TyDecl)
-    "$(t1.name)" == "$(t2.name)"
-end
+==(t1::TName, t2::TName) = t1.name == t2.name && (t1.qual == t2.qual || t1.qual == "" || t2.qual == "")
+==(t1::TUnionAll, t2::TUnionAll) = true
+==(t1::TType, t2::TType) = t1.t == t2.t
+==(t1::TSuperTuple, t2::TSuperTuple) = true
+==(t1::TValue, t2::TValue) = t1.v == t2.v
+==(t1::TyDecl, t2::TyDecl) = "$(t1.name)" == "$(t2.name)"
 
 import Base.hash
 
@@ -179,42 +135,30 @@ end
 
 import Base.show
 
-function show(io::IO, t::TAny)
-    print(io, "Any")
-end
-
-function show(io::IO, t::TUnion)
-    print_collection(io, t.ts, "Union{", "}")
-end
+show(io::IO, ::TAny) = print(io, "Any")
+show(io::IO, t::TUnion) = print_collection(io, t.ts, "Union{", "}")
 
 function show(io::IO, ts::Vector{ASTBase})
-    if length(ts) >= 2
-        map(t -> print(io, t, ", "), ts[1:end - 1])
+    if length(ts) >= 2; map(t -> print(io, t, ", "), ts[1:end - 1])
     end
-    if length(ts) >= 1
-        print(io, ts[end])
+    if length(ts) >= 1; print(io, ts[end])
     end
 end
 
-function show(io::IO, t::TApp)
-    print(io, t.t, "{", t.ts, "}")
-end
-
-function show(io::IO, t::TVar)
-    print(io, t.sym)
-end
+show(io::IO, t::TApp) = print(io, t.t, "{", t.ts, "}")
+show(io::IO, t::TVar) = print(io, t.sym)
 
 function show(io::IO, t::TWhere)
-    print(io, t.t)
+        print(io, t.t)
     print(io, " where ")
     if t.ub != TAny()
         if t.lb != EmptyUnion
             if isa(t.lb, TWhere) 
                 print(io, "(", t.lb, ")")
             else
-                print(io, t.lb)
+        print(io, t.lb)
             end
-            print(io, lj_showtype_xmlmode ? " &lt;: " : " <: ")
+        print(io, lj_showtype_xmlmode ? " &lt;: " : " <: ")
         end
         if t.diag print(io, "*") end  # diagonality marker # ROSSEXP
         print(io, t.tvar)
@@ -238,184 +182,63 @@ function show(io::IO, t::TWhere)
     end
 end
 
-function show(io::IO, t::TTuple)
-    print_collection(io, t.ts, "Tuple{", "}")
-end
+show(io::IO, t::TTuple) = print_collection(io, t.ts, "Tuple{", "}")
+show(io::IO, t::TName) = print(io, t.qual, isempty(t.qual) ? "" : "::", t.name)
+show(io::IO, ::TDataType) = print(io, "DataType")
+show(io::IO, ::TUnionAll) = print(io, "UnionAll")
+show(io::IO, ::TSuperUnion) = print(io, "Union")
+show(io::IO, t::TType) = print(io, "Type{", t.t, "}")
+show(io::IO, ::TSuperTuple) = print(io, "Tuple")
+show(io::IO, t::TValue) = print(io, t.v)
 
-function show(io::IO, t::TName)
-  # print(io, replace(String(t.name),"HHHH","#"))
-    print(io, t.qual, isempty(t.qual) ? "" : "::", t.name)
-end
+free_variables(t::ASTBase) = Vector{TVarSym}()
+free_variables(t::TUnion) = lj_flatten(vcat(map(ts1 -> free_variables(ts1), t.ts)))
+free_variables(t::TApp) = lj_flatten(vcat(free_variables(t.t), map(ts1 -> free_variables(ts1), t.ts)))
+free_variables(t::TVar) = [t.sym]
+free_variables(t::TWhere) = lj_flatten(vcat(Base.filter(v -> v != t.tvar.sym, free_variables(t.t)), free_variables(t.lb), free_variables(t.ub)))
+free_variables(t::TTuple) = lj_flatten(vcat(map(ts1 -> free_variables(ts1), t.ts)))
+free_variables(t::TUnionAll) = free_variables(t.t)
+free_variables(t::TType) = free_variables(t.t)
 
-function show(io::IO, t::TDataType)
-    print(io, "DataType")
-end
+all_variables(t::ASTBase) = Vector{TVarSym}()
+all_variables(t::TUnion) = lj_flatten(vcat(map(ts1 -> all_variables(ts1), t.ts)))
+all_variables(t::TApp) = lj_flatten(vcat(all_variables(t.t), map(ts1 -> all_variables(ts1), t.ts)))
+all_variables(t::TVar) = [t.sym]
+all_variables(t::TWhere) = lj_flatten(vcat(all_variables(t.t), all_variables(t.lb), all_variables(t.ub)))
+all_variables(t::TTuple) = lj_flatten(vcat(map(ts1 -> all_variables(ts1), t.ts)))
+all_variables(t::TUnionAll) = all_variables(t.t)
+all_variables(t::TType) = all_variables(t.t)
 
-function show(io::IO, t::TUnionAll)
-    print(io, "UnionAll")
-end
+is_kind(t::ASTBase) = isa(t, TDataType) || isa(t, TSuperUnion) || isa(t, TUnionAll) || t == TName("TypeofBottom", "Core")
 
-function show(io::IO, t::TSuperUnion)
-    print(io, "Union")
-end
+rename(t::ASTBase, on::TVar, nn::TVar) = substitute(t, on, nn)
 
-function show(io::IO, t::TType)
-    print(io, "Type{", t.t, "}")
-end
-
-function show(io::IO, t::TSuperTuple)
-    print(io, "Tuple")
-end
-
-function show(io::IO, t::TValue)
-    print(io, t.v)
-end
-
-# function show(io::IO, td::TyDecl)
-#    TO BE DONE
-# end
-
-
-##### free variables
-
-function free_variables(t::ASTBase)
-  # covers TAny, TName, TDataType, TSuperUnion
-    return Vector{TVarSym}()
-end
-
-function free_variables(t::TUnion)
-    return lj_flatten(vcat(map(ts1 -> free_variables(ts1), t.ts)))
-end
-
-function free_variables(t::TApp)
-    return lj_flatten(vcat(free_variables(t.t), map(ts1 -> free_variables(ts1), t.ts)))
-end
-
-function free_variables(t::TVar)
-    return [t.sym]
-end
-
-function free_variables(t::TWhere)
-    return lj_flatten(vcat(Base.filter(v -> v != t.tvar.sym, free_variables(t.t)),
-                 free_variables(t.lb), free_variables(t.ub)))
-end
-
-function free_variables(t::TTuple)
-    return lj_flatten(vcat(map(ts1 -> free_variables(ts1), t.ts)))
-end
-
-function free_variables(t::TUnionAll)
-    return free_variables(t.t)
-end
-
-function free_variables(t::TType)
-    return free_variables(t.t)
-end
-
-##### all variables
-
-function all_variables(t::ASTBase)
-  # covers TAny, TName, TDataType, TSuperUnion
-    return Vector{TVarSym}()
-end
-
-function all_variables(t::TUnion)
-    return lj_flatten(vcat(map(ts1 -> all_variables(ts1), t.ts)))
-end
-
-function all_variables(t::TApp)
-    return lj_flatten(vcat(all_variables(t.t), map(ts1 -> all_variables(ts1), t.ts)))
-end
-
-function all_variables(t::TVar)
-    return [t.sym]
-end
-
-function all_variables(t::TWhere)
-    return lj_flatten(vcat(all_variables(t.t),
-                         all_variables(t.lb), all_variables(t.ub)))
-end
-
-function all_variables(t::TTuple)
-    return lj_flatten(vcat(map(ts1 -> all_variables(ts1), t.ts)))
-end
-
-function all_variables(t::TUnionAll)
-    return all_variables(t.t)
-end
-
-function all_variables(t::TType)
-    return all_variables(t.t)
-end
-
-
-function is_kind(t::ASTBase)
-    isa(t, TDataType) || isa(t, TSuperUnion) || isa(t, TUnionAll) || 
-  t == TName("TypeofBottom", "Core")
-end
-
-function rename(t::ASTBase, on::TVar, nn::TVar)
-    return substitute(t, on, nn)
-end
-
-function substitute(t::ASTBase, on::TVar, t1::ASTBase)
-    @assert any(tbase -> isa(t, tbase), 
-              [TAny, TName, TDataType, TSuperUnion, TSuperTuple, TValue])
+function substitute(t::ASTBase, ::TVar, ::ASTBase)
+    @assert any(tbase -> isa(t, tbase), [TAny, TName, TDataType, TSuperUnion, TSuperTuple, TValue])
     return t
 end
-
-function substitute(t::TUnion, on::TVar, t1::ASTBase) # including TUnion([]) ~= TBottom
-    return TUnion(map(ts1 -> substitute(ts1, on, t1), t.ts))
-end
-
-function substitute(t::TApp, on::TVar, t1::ASTBase)
-    return TApp(substitute(t.t, on, t1),  map(ts1 -> substitute(ts1, on, t1), t.ts))
-end
-
-function substitute(t::TVar, on::TVar, t1::ASTBase)
-    if t.sym == on.sym
-        return t1
-    else
-        return t
-    end
-end
-
+substitute(t::TUnion, on::TVar, t1::ASTBase) = TUnion(map(ts1 -> substitute(ts1, on, t1), t.ts))
+substitute(t::TApp, on::TVar, t1::ASTBase) = TApp(substitute(t.t, on, t1),  map(ts1 -> substitute(ts1, on, t1), t.ts))
+substitute(t::TVar, on::TVar, t1::ASTBase) = t.sym == on.sym ? t1 : t
+substitute(t::TTuple, on::TVar, t1::ASTBase) = TTuple(map(ts1 -> substitute(ts1, on, t1), t.ts))
+substitute(t::TUnionAll, on::TVar, t1::ASTBase) = TUnionAll(substitute(t.t, on, t1))
+substitute(t::TType, on::TVar, t1::ASTBase) = TType(substitute(t.t, on, t1))
 function substitute(t::TWhere, on::TVar, t1::ASTBase)
-    if t.tvar.sym == on.sym
-        return TWhere(t.t, t.tvar, substitute(t.lb, on, t1), substitute(t.ub, on, t1))
+    if t.tvar.sym == on.sym; return TWhere(t.t, t.tvar, substitute(t.lb, on, t1), substitute(t.ub, on, t1))
     end
-    return TWhere(substitute(t.t, on, t1),
-                t.tvar, substitute(t.lb, on, t1), substitute(t.ub, on, t1))
-end
-
-function substitute(t::TTuple, on::TVar, t1::ASTBase)
-    return TTuple(map(ts1 -> substitute(ts1, on, t1), t.ts))
-end
-
-function substitute(t::TUnionAll, on::TVar, t1::ASTBase)
-    return TUnionAll(substitute(t.t, on, t1))
-end
-
-function substitute(t::TType, on::TVar, t1::ASTBase)
-    return TType(substitute(t.t, on, t1))
+    TWhere(substitute(t.t, on, t1), t.tvar, substitute(t.lb, on, t1), substitute(t.ub, on, t1))
 end
 
 betared(t::Union{TName,TAny,TDataType,TUnionAll,TSuperUnion,TSuperTuple,TValue,TVar}) = t
-
 betared(t::Union{TUnion,TTuple}) = typeof(t)(map(betared, t.ts))
-
 betared(t::TType) = TType(betared(t.t))
-
 betared(t::TWhere) = TWhere(betared(t.t), t.tvar, t.lb, t.ub)
-
 function betared(t::TApp)
-    if length(t.ts) == 0
-        return betared(t.t)
+    if length(t.ts) == 0; return betared(t.t)
     elseif isa(t.t, TWhere)
         tnew = substitute(t.t.t, t.t.tvar, t.ts[1])
         return betared(TApp(tnew, t.ts[2:end]))
-    else
-        return TApp(betared(t.t), map(betared, t.ts))
     end
+    TApp(betared(t.t), map(betared, t.ts))
 end
 
