b85c44d5c534953f6c2e4d0107a77ed488f95716
quantapix <quantapix@gmail.com>
2021-03-24 00:17:52 -0400

    qpx

--- semcode/src/extension.ts
+++ semcode/src/extension.ts
@@ -1,6 +1,3 @@
-'use strict';
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import { SeverityLevel } from 'applicationinsights/out/Declarations/Contracts';
 import * as fs from 'async-file';
 import { unwatchFile, watchFile } from 'async-file';
@@ -28,56 +25,41 @@ let g_context: vscode.ExtensionContext = null;
 let g_watchedEnvironmentFile: string = null;
 let g_startupNotification: vscode.StatusBarItem = null;
 
-export async function activate(context: vscode.ExtensionContext) {
+export async function activate(ctx: vscode.ExtensionContext) {
   if (vscode.extensions.getExtension('julialang.language-julia') && vscode.extensions.getExtension('julialang.language-julia-insider')) {
     vscode.window.showErrorMessage(
       'You have both the Julia Insider and regular Julia extension installed at the same time, which is not supported. Please uninstall or disable one of the two extensions.'
     );
     return;
   }
-
-  await telemetry.init(context);
+  await telemetry.init(ctx);
   try {
     telemetry.traceEvent('activate');
-
     telemetry.startLsCrashServer();
-
-    g_context = context;
-
+    g_context = ctx;
     console.log('Activating extension language-julia');
-
-    // Config change
-    context.subscriptions.push(vscode.workspace.onDidChangeConfiguration(changeConfig));
-
-    // Language settings
+    ctx.subscriptions.push(vscode.workspace.onDidChangeConfiguration(changeConfig));
     vscode.languages.setLanguageConfiguration('julia', {
       indentationRules: {
         increaseIndentPattern: /^(\s*|.*=\s*|.*@\w*\s*)[\w\s]*(?:["'`][^"'`]*["'`])*[\w\s]*\b(if|while|for|function|macro|(mutable\s+)?struct|abstract\s+type|primitive\s+type|let|quote|try|begin|.*\)\s*do|else|elseif|catch|finally)\b(?!(?:.*\bend\b[^\]]*)|(?:[^\[]*\].*)$).*$/,
         decreaseIndentPattern: /^\s*(end|else|elseif|catch|finally)\b.*$/,
       },
     });
-
-    // Active features from other files
-    juliaexepath.activate(context);
-    await juliaexepath.getJuliaExePath(); // We run this function now and await to make sure we don't run in twice simultaneously later
-    repl.activate(context);
-    weave.activate(context);
-    documentation.activate(context);
-    tasks.activate(context);
-    smallcommands.activate(context);
-    packagepath.activate(context);
-    openpackagedirectory.activate(context);
-    jlpkgenv.activate(context);
-
-    context.subscriptions.push(new JuliaDebugFeature(context));
-    context.subscriptions.push(new JuliaPackageDevFeature(context));
-
+    juliaexepath.activate(ctx);
+    await juliaexepath.getJuliaExePath();
+    repl.activate(ctx);
+    weave.activate(ctx);
+    documentation.activate(ctx);
+    tasks.activate(ctx);
+    smallcommands.activate(ctx);
+    packagepath.activate(ctx);
+    openpackagedirectory.activate(ctx);
+    jlpkgenv.activate(ctx);
+    ctx.subscriptions.push(new JuliaDebugFeature(ctx));
+    ctx.subscriptions.push(new JuliaPackageDevFeature(ctx));
     g_startupNotification = vscode.window.createStatusBarItem();
-    context.subscriptions.push(g_startupNotification);
-
-    // Start language server
+    ctx.subscriptions.push(g_startupNotification);
     startLanguageServer();
-
     if (vscode.workspace.getConfiguration('julia').get<boolean>('enableTelemetry') === null) {
       const agree = 'Yes';
       const disagree = 'No';
@@ -95,15 +77,11 @@ export async function activate(context: vscode.ExtensionContext) {
           }
         });
     }
-
-    context.subscriptions.push(
-      // commands
+    ctx.subscriptions.push(
       registerCommand('language-julia.refreshLanguageServer', refreshLanguageServer),
       registerCommand('language-julia.restartLanguageServer', restartLanguageServer),
-      // registries
       vscode.workspace.registerTextDocumentContentProvider('juliavsodeprofilerresults', new ProfilerResultsProvider())
     );
-
     const api = {
       version: 2,
       async getEnvironment() {
@@ -116,7 +94,6 @@ export async function activate(context: vscode.ExtensionContext) {
         return vscode.workspace.getConfiguration('julia').get('packageServer');
       },
     };
-
     return api;
   } catch (err) {
     telemetry.handleNewCrashReportFromException(err, 'Extension');
@@ -124,23 +101,20 @@ export async function activate(context: vscode.ExtensionContext) {
   }
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() {}
 
 const g_onSetLanguageClient = new vscode.EventEmitter<LanguageClient>();
 export const onSetLanguageClient = g_onSetLanguageClient.event;
-function setLanguageClient(languageClient: LanguageClient = null) {
-  g_onSetLanguageClient.fire(languageClient);
-  g_languageClient = languageClient;
+function setLanguageClient(c?: LanguageClient) {
+  g_onSetLanguageClient.fire(c);
+  g_languageClient = c;
 }
 
-export async function withLanguageClient(callback: (languageClient: LanguageClient) => any, callbackOnHandledErr: (err: Error) => any) {
+export async function withLanguageClient(callback: (c: LanguageClient) => any, callbackOnHandledErr: (err: Error) => any) {
   if (g_languageClient === null) {
     return callbackOnHandledErr(new Error('Language client is not active'));
   }
-
   await g_languageClient.onReady();
-
   try {
     return callback(g_languageClient);
   } catch (err) {
@@ -229,32 +203,26 @@ async function startLanguageServer() {
     middleware: {
       provideCompletionItem: async (document, position, context, token, next) => {
         const validatedPosition = document.validatePosition(position);
-
         if (validatedPosition !== position) {
           telemetry.traceTrace({
             message: `Middleware found a change in position in provideCompletionItem. Original ${position.line}:${position.character}, validated ${validatedPosition.line}:${validatedPosition.character}`,
             severity: SeverityLevel.Error,
           });
         }
-
         return await next(document, position, context, token);
       },
       provideDefinition: async (document, position, token, next) => {
         const validatedPosition = document.validatePosition(position);
-
         if (validatedPosition !== position) {
           telemetry.traceTrace({
             message: `Middleware found a change in position in provideDefinition. Original ${position.line}:${position.character}, validated ${validatedPosition.line}:${validatedPosition.character}`,
             severity: SeverityLevel.Error,
           });
         }
-
         return await next(document, position, token);
       },
     },
   };
-
-  // Create the language client and start the client.
   const languageClient = new LanguageClient('julia', 'Julia Language Server', serverOptions, clientOptions);
   languageClient.registerProposedFeatures();
   languageClient.onTelemetry((data: any) => {
@@ -267,30 +235,24 @@ async function startLanguageServer() {
       telemetry.tracePackageLoadError(data.name, data.message);
     }
   });
-
   if (g_watchedEnvironmentFile) {
     unwatchFile(g_watchedEnvironmentFile);
   }
-
-  // automatic environement refreshing
   g_watchedEnvironmentFile = (await jlpkgenv.getProjectFilePaths(jlEnvPath)).manifest_toml_path;
-  // polling watch for robustness
   if (g_watchedEnvironmentFile) {
     watchFile(g_watchedEnvironmentFile, { interval: 10000 }, (curr, prev) => {
       if (curr.mtime > prev.mtime) {
         if (!languageClient.needsStop()) {
           return;
-        } // this client already gets stopped
+        }
         refreshLanguageServer(languageClient);
       }
     });
   }
-
   const disposable = registerCommand('language-julia.showLanguageServerOutput', () => {
     languageClient.outputChannel.show(true);
   });
   try {
-    // Push the disposable to the context's subscriptions so that the  client can be deactivated on extension deactivation
     g_context.subscriptions.push(languageClient.start());
     g_startupNotification.command = 'language-julia.showLanguageServerOutput';
     setLanguageClient(languageClient);
@@ -306,16 +268,14 @@ async function startLanguageServer() {
   }
 }
 
-function refreshLanguageServer(languageClient: LanguageClient = g_languageClient) {
-  if (!languageClient) {
-    return;
-  }
-  languageClient.sendNotification('julia/refreshLanguageServer');
+function refreshLanguageServer(c: LanguageClient = g_languageClient) {
+  if (!c) return;
+  c.sendNotification('julia/refreshLanguageServer');
 }
 
-function restartLanguageServer(languageClient: LanguageClient = g_languageClient) {
-  if (languageClient !== null) {
-    languageClient.stop();
+function restartLanguageServer(c: LanguageClient = g_languageClient) {
+  if (c !== null) {
+    c.stop();
     setLanguageClient();
   }
   startLanguageServer();
