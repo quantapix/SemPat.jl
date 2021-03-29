674f5336fd6c8046bc4be85b72b030307532faf4
quantapix <quantapix@gmail.com>
2021-03-29 16:27:10 -0400

    qpx

--- semcode/direct/src/extension-old.ts
+++ semcode/direct/src/extension-old.ts
@@ -1,44 +1,36 @@
-/*---------------------------------------------------------------------------------------------
- *  Copyright (c) Microsoft Corporation. All rights reserved.
- *  Licensed under the MIT License. See License.txt in the project root for license information.
- *--------------------------------------------------------------------------------------------*/
-
-import * as fs from 'fs';
-import * as qv from 'vscode';
-import { Api, getExtensionApi } from './api';
-import { CommandManager } from './commands/commandManager';
-import { registerBaseCommands } from './commands';
-import { LanguageConfigurationManager } from './languageFeatures/languageConfiguration';
-import { createLazyClientHost, lazilyActivateClient } from './lazyClientHost';
-import { nodeRequestCancellerFactory } from './tsServer/cancellation.electron';
-import { NodeLogDirectoryProvider } from './tsServer/logDirectoryProvider.electron';
-import { ChildServerProcess } from './tsServer/serverProcess.electron';
-import { DiskTypeScriptVersionProvider } from './tsServer/versionProvider.electron';
 import { ActiveJsTsEditorTracker } from './utils/activeJsTsEditorTracker';
+import { ActiveJsTsEditorTracker } from './utils/activeJsTsEditorTracker';
+import { Api, getExtensionApi } from './api';
+import { ChildServerProcess } from './tsServer/serverProcess.electron';
+import { CommandManager } from './commands/commandManager';
+import { createLazyClientHost, lazilyActivateClient } from './lazyClientHost';
+import { DiskTypeScriptVersionProvider } from './tsServer/versionProvider.electron';
+import { ITypeScriptVersionProvider, TypeScriptVersion } from './tsServer/versionProvider';
+import { LanguageConfigurationManager } from './languageFeatures/languageConfiguration';
+import { NodeLogDirectoryProvider } from './tsServer/logDirectoryProvider.electron';
+import { nodeRequestCancellerFactory } from './tsServer/cancellation.electron';
 import { onCaseInsenitiveFileSystem } from './utils/fileSystem.electron';
 import { PluginManager } from './utils/plugins';
+import { registerBaseCommands } from './commands';
+import { TypeScriptServiceConfiguration } from './utils/configuration';
+import * as fs from 'fs';
+import * as qv from 'vscode';
 import * as temp from './utils/temp.electron';
 
-export function activate(context: qv.ExtensionContext): Api {
+export function activate(ctx: qv.ExtensionContext): Api {
   const pluginManager = new PluginManager();
-  context.subscriptions.push(pluginManager);
-
+  ctx.subscriptions.push(pluginManager);
   const commandManager = new CommandManager();
-  context.subscriptions.push(commandManager);
-
+  ctx.subscriptions.push(commandManager);
   const onCompletionAccepted = new qv.EventEmitter<qv.CompletionItem>();
-  context.subscriptions.push(onCompletionAccepted);
-
-  const logDirectoryProvider = new NodeLogDirectoryProvider(context);
+  ctx.subscriptions.push(onCompletionAccepted);
+  const logDirectoryProvider = new NodeLogDirectoryProvider(ctx);
   const versionProvider = new DiskTypeScriptVersionProvider();
-
-  context.subscriptions.push(new LanguageConfigurationManager());
-
+  ctx.subscriptions.push(new LanguageConfigurationManager());
   const activeJsTsEditorTracker = new ActiveJsTsEditorTracker();
-  context.subscriptions.push(activeJsTsEditorTracker);
-
+  ctx.subscriptions.push(activeJsTsEditorTracker);
   const lazyClientHost = createLazyClientHost(
-    context,
+    ctx,
     onCaseInsenitiveFileSystem(),
     {
       pluginManager,
@@ -53,19 +45,14 @@ export function activate(context: qv.ExtensionContext): Api {
       onCompletionAccepted.fire(item);
     }
   );
-
   registerBaseCommands(commandManager, lazyClientHost, pluginManager, activeJsTsEditorTracker);
-
-  import('./providers/task').then((module) => {
-    context.subscriptions.push(module.register(lazyClientHost.map((x) => x.serviceClient)));
+  import('./providers/task').then((m) => {
+    ctx.subscriptions.push(m.register(lazyClientHost.map((x) => x.serviceClient)));
   });
-
-  import('./languageFeatures/tsconfig').then((module) => {
-    context.subscriptions.push(module.register());
+  import('./languageFeatures/tsconfig').then((m) => {
+    ctx.subscriptions.push(m.register());
   });
-
-  context.subscriptions.push(lazilyActivateClient(lazyClientHost, pluginManager, activeJsTsEditorTracker));
-
+  ctx.subscriptions.push(lazilyActivateClient(lazyClientHost, pluginManager, activeJsTsEditorTracker));
   return getExtensionApi(onCompletionAccepted.event, pluginManager);
 }
 
@@ -73,89 +60,18 @@ export function deactivate() {
   fs.rmdirSync(temp.getInstanceTempDir(), { recursive: true });
 }
 
-/*---------------------------------------------------------------------------------------------
- *  Browser
- *--------------------------------------------------------------------------------------------*/
-
-import * as qv from 'vscode';
-import { Api, getExtensionApi } from './api';
-import { registerBaseCommands } from './commands';
-import { LanguageConfigurationManager } from './languageFeatures/languageConfiguration';
-import { createLazyClientHost, lazilyActivateClient } from './lazyClientHost';
-import { noopRequestCancellerFactory } from './tsServer/cancellation';
-import { noopLogDirectoryProvider } from './tsServer/logDirectoryProvider';
-import { ITypeScriptVersionProvider, TypeScriptVersion, TypeScriptVersionSource } from './tsServer/versionProvider';
-import { WorkerServerProcess } from './tsServer/serverProcess.browser';
-import API from './utils/api';
-import { CommandManager } from './commands/commandManager';
-import { TypeScriptServiceConfiguration } from './utils/configuration';
-import { PluginManager } from './utils/plugins';
-import { ActiveJsTsEditorTracker } from './utils/activeJsTsEditorTracker';
-
 class StaticVersionProvider implements ITypeScriptVersionProvider {
   constructor(private readonly _version: TypeScriptVersion) {}
-
   updateConfiguration(_configuration: TypeScriptServiceConfiguration): void {
     // noop
   }
-
   get defaultVersion() {
     return this._version;
   }
   get bundledVersion() {
     return this._version;
   }
-
   readonly globalVersion = undefined;
   readonly localVersion = undefined;
   readonly localVersions = [];
-}
-
-export function activate(context: qv.ExtensionContext): Api {
-  const pluginManager = new PluginManager();
-  context.subscriptions.push(pluginManager);
-
-  const commandManager = new CommandManager();
-  context.subscriptions.push(commandManager);
-
-  context.subscriptions.push(new LanguageConfigurationManager());
-
-  const onCompletionAccepted = new qv.EventEmitter<qv.CompletionItem>();
-  context.subscriptions.push(onCompletionAccepted);
-
-  const activeJsTsEditorTracker = new ActiveJsTsEditorTracker();
-  context.subscriptions.push(activeJsTsEditorTracker);
-
-  const versionProvider = new StaticVersionProvider(
-    new TypeScriptVersion(TypeScriptVersionSource.Bundled, qv.Uri.joinPath(context.extensionUri, 'dist/browser/typescript/tsserver.web.js').toString(), API.fromSimpleString('4.2.0'))
-  );
-
-  const lazyClientHost = createLazyClientHost(
-    context,
-    false,
-    {
-      pluginManager,
-      commandManager,
-      logDirectoryProvider: noopLogDirectoryProvider,
-      cancellerFactory: noopRequestCancellerFactory,
-      versionProvider,
-      processFactory: WorkerServerProcess,
-      activeJsTsEditorTracker,
-    },
-    (item) => {
-      onCompletionAccepted.fire(item);
-    }
-  );
-
-  registerBaseCommands(commandManager, lazyClientHost, pluginManager, activeJsTsEditorTracker);
-
-  // context.subscriptions.push(task.register(lazyClientHost.map(x => x.serviceClient)));
-
-  import('./languageFeatures/tsconfig').then((module) => {
-    context.subscriptions.push(module.register());
-  });
-
-  context.subscriptions.push(lazilyActivateClient(lazyClientHost, pluginManager, activeJsTsEditorTracker));
-
-  return getExtensionApi(onCompletionAccepted.event, pluginManager);
 }
--- semcode/direct/src/features/token.ts
+++ semcode/direct/src/features/token.ts
@@ -2,6 +2,7 @@ import { ClientCap, ExecConfig, ServiceClient, ServerResponse } from '../service
 import { condRegistration, requireSomeCap, requireMinVer } from '../registration';
 import * as qp from '../protocol';
 import * as qv from 'vscode';
+import * as qu from '../utils';
 import API from '../../old/ts/utils/api';
 
 const minTypeScriptVersion = API.fromVersionString(`${VersionRequirement.major}.${VersionRequirement.minor}`);
@@ -10,12 +11,12 @@ const CONTENT_LENGTH_LIMIT = 100000;
 
 export function register(s: qu.DocumentSelector, c: ServiceClient) {
   return condRegistration([requireMinVer(c, minTypeScriptVersion), requireSomeCap(c, ClientCap.Semantic)], () => {
-    const p = new DocumentSemanticTokensProvider(c);
+    const p = new SemanticTokens(c);
     return qv.Disposable.from(qv.languages.registerDocumentRangeSemanticTokensProvider(s.semantic, p, p.getLegend()));
   });
 }
 
-class DocumentSemanticTokensProvider implements qv.DocumentSemanticTokensProvider, qv.DocumentRangeSemanticTokensProvider {
+class SemanticTokens implements qv.DocumentSemanticTokensProvider, qv.DocumentRangeSemanticTokensProvider {
   constructor(private readonly client: ServiceClient) {}
 
   getLegend(): qv.SemanticTokensLegend {
@@ -25,7 +26,6 @@ class DocumentSemanticTokensProvider implements qv.DocumentSemanticTokensProvide
   async provideDocumentSemanticTokens(document: qv.TextDocument, token: qv.CancellationToken): Promise<qv.SemanticTokens | null> {
     const file = this.client.toOpenedFilePath(document);
     if (!file || document.getText().length > CONTENT_LENGTH_LIMIT) return null;
-
     return this._provideSemanticTokens(document, { file, start: 0, length: document.getText().length }, token);
   }
 
@@ -236,5 +236,115 @@ namespace ExperimentalProtocol {
 
   export interface ExtendedTsServerRequests {
     'encodedSemanticClassifications-full': [ExperimentalProtocol.EncodedSemanticClassificationsRequestArgs, ExperimentalProtocol.EncodedSemanticClassificationsResponse];
+  }
+}
+
+const tokenTypes = new Map<string, number>();
+const tokenModifiers = new Map<string, number>();
+
+const legend = (function () {
+  const tokenTypesLegend = [
+    'comment',
+    'string',
+    'keyword',
+    'number',
+    'regexp',
+    'operator',
+    'namespace',
+    'type',
+    'struct',
+    'class',
+    'interface',
+    'enum',
+    'typeParameter',
+    'function',
+    'method',
+    'macro',
+    'variable',
+    'parameter',
+    'property',
+    'label',
+  ];
+  tokenTypesLegend.forEach((tokenType, index) => tokenTypes.set(tokenType, index));
+  const tokenModifiersLegend = ['declaration', 'documentation', 'readonly', 'static', 'abstract', 'deprecated', 'modification', 'async'];
+  tokenModifiersLegend.forEach((tokenModifier, index) => tokenModifiers.set(tokenModifier, index));
+  return new qv.SemanticTokensLegend(tokenTypesLegend, tokenModifiersLegend);
+})();
+
+export function activate(context: qv.ExtensionContext) {
+  context.subscriptions.push(qv.languages.registerDocumentSemanticTokensProvider({ language: 'semanticLanguage' }, new DocumentSemanticTokensProvider(), legend));
+}
+
+interface IParsedToken {
+  line: number;
+  startCharacter: number;
+  length: number;
+  tokenType: string;
+  tokenModifiers: string[];
+}
+
+class DocumentSemanticTokensProvider implements qv.DocumentSemanticTokensProvider {
+  async provideDocumentSemanticTokens(document: qv.TextDocument, token: qv.CancellationToken): Promise<qv.SemanticTokens> {
+    const allTokens = this._parseText(document.getText());
+    const builder = new qv.SemanticTokensBuilder();
+    allTokens.forEach((token) => {
+      builder.push(token.line, token.startCharacter, token.length, this._encodeTokenType(token.tokenType), this._encodeTokenModifiers(token.tokenModifiers));
+    });
+    return builder.build();
+  }
+
+  private _encodeTokenType(tokenType: string): number {
+    if (tokenTypes.has(tokenType)) {
+      return tokenTypes.get(tokenType)!;
+    } else if (tokenType === 'notInLegend') {
+      return tokenTypes.size + 2;
+    }
+    return 0;
+  }
+
+  private _encodeTokenModifiers(strTokenModifiers: string[]): number {
+    let result = 0;
+    for (let i = 0; i < strTokenModifiers.length; i++) {
+      const tokenModifier = strTokenModifiers[i];
+      if (tokenModifiers.has(tokenModifier)) {
+        result = result | (1 << tokenModifiers.get(tokenModifier)!);
+      } else if (tokenModifier === 'notInLegend') {
+        result = result | (1 << (tokenModifiers.size + 2));
+      }
+    }
+    return result;
+  }
+
+  private _parseText(text: string): IParsedToken[] {
+    const r: IParsedToken[] = [];
+    const lines = text.split(/\r\n|\r|\n/);
+    for (let i = 0; i < lines.length; i++) {
+      const line = lines[i];
+      let currentOffset = 0;
+      do {
+        const openOffset = line.indexOf('[', currentOffset);
+        if (openOffset === -1) break;
+        const closeOffset = line.indexOf(']', openOffset);
+        if (closeOffset === -1) break;
+        const tokenData = this._parseTextToken(line.substring(openOffset + 1, closeOffset));
+        r.push({
+          line: i,
+          startCharacter: openOffset + 1,
+          length: closeOffset - openOffset - 1,
+          tokenType: tokenData.tokenType,
+          tokenModifiers: tokenData.tokenModifiers,
+        });
+        currentOffset = closeOffset;
+      } while (true);
+    }
+    return r;
+  }
+
+  private _parseTextToken(text: string): { tokenType: string; tokenModifiers: string[] } {
+    const parts = text.split('.');
+    return {
+      tokenType: parts[0],
+      tokenModifiers: parts.slice(1),
+    };
   }
 }
