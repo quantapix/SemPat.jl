761b1c777b61181d93a5e0ee3a0d1d630ea1d49a
quantapix <quantapix@gmail.com>
2021-03-29 16:55:28 -0400

    qpx

--- semcode/direct/src/extension-old.ts
+++ semcode/direct/src/extension-old.ts
@@ -16,8 +16,22 @@ import { TypeScriptServiceConfiguration } from './utils/configuration';
 import * as fs from 'fs';
 import * as qv from 'vscode';
 import * as temp from './utils/temp.electron';
+import { basename } from 'path';
 
 export function activate(ctx: qv.ExtensionContext): Api {
+  const stat = qv.window.createStatusBarItem(qv.StatusBarAlignment.Left, 1000000);
+  ctx.subscriptions.push(stat);
+  ctx.subscriptions.push(qv.workspace.onDidChangeWorkspaceFolders((e) => updateStatus(stat)));
+  ctx.subscriptions.push(qv.workspace.onDidChangeConfiguration((e) => updateStatus(stat)));
+  ctx.subscriptions.push(qv.window.onDidChangeActiveTextEditor((e) => updateStatus(stat)));
+  ctx.subscriptions.push(qv.window.onDidChangeTextEditorViewColumn((e) => updateStatus(stat)));
+  ctx.subscriptions.push(qv.workspace.onDidOpenTextDocument((e) => updateStatus(stat)));
+  ctx.subscriptions.push(qv.workspace.onDidCloseTextDocument((e) => updateStatus(stat)));
+  updateStatus(stat);
+
+	ctx.subscriptions.push(qv.languages.registerCallHierarchyProvider('plaintext', new FoodPyramid());
+	showSampleText(ctx);
+
   const pluginManager = new PluginManager();
   ctx.subscriptions.push(pluginManager);
   const commandManager = new CommandManager();
@@ -58,6 +72,42 @@ export function activate(ctx: qv.ExtensionContext): Api {
 
 export function deactivate() {
   fs.rmdirSync(temp.getInstanceTempDir(), { recursive: true });
+}
+
+function updateStatus(s: qv.StatusBarItem): void {
+  const i = getEditorInfo();
+  s.text = i ? i.text || '' : '';
+  s.tooltip = i ? i.tooltip : undefined;
+  s.color = i ? i.color : undefined;
+  if (i) s.show();
+  else s.hide();
+}
+
+function getEditorInfo(): { text?: string; tooltip?: string; color?: string } | undefined {
+  const e = qv.window.activeTextEditor;
+  if (!e || !qv.workspace.workspaceFolders || qv.workspace.workspaceFolders.length < 2) return null;
+  let text: string | undefined;
+  let tooltip: string | undefined;
+  let color: string | undefined;
+  const r = e.document.uri;
+  if (r.scheme === 'file') {
+    const f = qv.workspace.getWorkspaceFolder(r);
+    if (!f) text = `$(alert) <outside workspace> → ${basename(r.fsPath)}`;
+    else {
+      text = `$(file-submodule) ${basename(f.uri.fsPath)} (${f.index + 1} of ${qv.workspace.workspaceFolders.length}) → $(file-code) ${basename(r.fsPath)}`;
+      tooltip = r.fsPath;
+      const c = qv.workspace.getConfiguration('multiRootSample', r);
+      color = c.get('statusColor');
+    }
+  }
+  return { text, tooltip, color };
+}
+
+async function showSampleText(ctx: qv.ExtensionContext): Promise<void> {
+	const e = await qv.workspace.fs.readFile(qv.Uri.file(ctx.asAbsolutePath('sample.txt')));
+	const t = new TextDecoder('utf-8').decode(e);
+	const d = await qv.workspace.openTextDocument({ language: 'plaintext', content: t });
+	qv.window.showTextDocument(d);
 }
 
 class StaticVersionProvider implements ITypeScriptVersionProvider {
---
+++ semcode/direct/src/features/FoodPyramid.ts
@@ -0,0 +1,174 @@
+import * as qv from 'vscode';
+
+export class FoodPyramidHierarchy implements qv.CallHierarchyProvider {
+  prepareCallHierarchy(document: qv.TextDocument, position: qv.Position, token: qv.CancellationToken): qv.CallHierarchyItem | undefined {
+    const range = document.getWordRangeAtPosition(position);
+    if (range) {
+      const word = document.getText(range);
+      return this.createCallHierarchyItem(word, '', document, range);
+    } else {
+      return undefined;
+    }
+  }
+
+  async provideCallHierarchyOutgoingCalls(item: qv.CallHierarchyItem, token: qv.CancellationToken): Promise<qv.CallHierarchyOutgoingCall[] | undefined> {
+    const document = await qv.workspace.openTextDocument(item.uri);
+    const parser = new FoodPyramidParser();
+    parser.parse(document);
+    const model = parser.getModel();
+    const originRelation = model.getRelationAt(item.range);
+    const outgoingCallItems: qv.CallHierarchyOutgoingCall[] = [];
+    if (model.isVerb(item.name)) {
+      const outgoingCalls = model.getVerbRelations(item.name).filter((relation) => relation.subject === originRelation!.subject);
+      outgoingCalls.forEach((relation) => {
+        const outgoingCallRange = relation.getRangeOf(relation.object);
+        const verbItem = this.createCallHierarchyItem(relation.object, 'noun', document, outgoingCallRange);
+        const outgoingCallItem = new qv.CallHierarchyOutgoingCall(verbItem, [outgoingCallRange]);
+        outgoingCallItems.push(outgoingCallItem);
+      });
+    } else if (model.isNoun(item.name)) {
+      const outgoingCallMap = groupBy(model.getSubjectRelations(item.name), (relation) => relation.verb);
+      outgoingCallMap.forEach((relations, verb) => {
+        const outgoingCallRanges = relations.map((relation) => relation.getRangeOf(verb));
+        const verbItem = this.createCallHierarchyItem(verb, 'verb', document, outgoingCallRanges[0]);
+        const outgoingCallItem = new qv.CallHierarchyOutgoingCall(verbItem, outgoingCallRanges);
+        outgoingCallItems.push(outgoingCallItem);
+      });
+    }
+    return outgoingCallItems;
+  }
+
+  async provideCallHierarchyIncomingCalls(item: qv.CallHierarchyItem, token: qv.CancellationToken): Promise<qv.CallHierarchyIncomingCall[]> {
+    const document = await qv.workspace.openTextDocument(item.uri);
+    const parser = new FoodPyramidParser();
+    parser.parse(document);
+    const model = parser.getModel();
+    const originRelation = model.getRelationAt(item.range);
+    const outgoingCallItems: qv.CallHierarchyIncomingCall[] = [];
+    if (model.isVerb(item.name)) {
+      const outgoingCalls = model.getVerbRelations(item.name).filter((relation) => relation.object === originRelation!.object);
+      outgoingCalls.forEach((relation) => {
+        const outgoingCallRange = relation.getRangeOf(relation.subject);
+        const verbItem = this.createCallHierarchyItem(relation.subject, 'noun', document, outgoingCallRange);
+        const outgoingCallItem = new qv.CallHierarchyIncomingCall(verbItem, [outgoingCallRange]);
+        outgoingCallItems.push(outgoingCallItem);
+      });
+    } else if (model.isNoun(item.name)) {
+      const outgoingCallMap = groupBy(model.getObjectRelations(item.name), (relation) => relation.verb);
+      outgoingCallMap.forEach((relations, verb) => {
+        const outgoingCallRanges = relations.map((relation) => relation.getRangeOf(verb));
+        const verbItem = this.createCallHierarchyItem(verb, 'verb-inverted', document, outgoingCallRanges[0]);
+        const outgoingCallItem = new qv.CallHierarchyIncomingCall(verbItem, outgoingCallRanges);
+        outgoingCallItems.push(outgoingCallItem);
+      });
+    }
+    return outgoingCallItems;
+  }
+
+  private createCallHierarchyItem(word: string, type: string, document: qv.TextDocument, range: qv.Range): qv.CallHierarchyItem {
+    return new qv.CallHierarchyItem(qv.SymbolKind.Object, word, `(${type})`, document.uri, range, range);
+  }
+}
+
+class FoodPyramidParser {
+  private _model = new FoodPyramid();
+
+  getModel(): FoodPyramid {
+    return this._model;
+  }
+
+  parse(textDocument: qv.TextDocument): void {
+    const pattern = /^(\w+)\s+(\w+)\s+(\w+).$/gm;
+    let match: RegExpExecArray | null;
+    while ((match = pattern.exec(textDocument.getText()))) {
+      const startPosition = textDocument.positionAt(match.index);
+      const range = new qv.Range(startPosition, startPosition.translate({ characterDelta: match[0].length }));
+      this._model.addRelation(new FoodRelation(match[1], match[2], match[3], match[0], range));
+    }
+  }
+}
+
+function groupBy<K, V>(array: Array<V>, keyGetter: (value: V) => K): Map<K, V[]> {
+  const map = new Map();
+  array.forEach((item) => {
+    const key = keyGetter(item);
+    const groupForKey = map.get(key) || [];
+    groupForKey.push(item);
+    map.set(key, groupForKey);
+  });
+  return map;
+}
+
+export class FoodPyramid {
+  private _relations: FoodRelation[] = [];
+  private _nouns = new Set<string>();
+  private _verbs = new Set<string>();
+
+  getRelationAt(wordRange: qv.Range): FoodRelation | undefined {
+    return this._relations.find((relation) => relation.range.contains(wordRange));
+  }
+
+  addRelation(relation: FoodRelation): void {
+    this._relations.push(relation);
+    this._nouns.add(relation.object).add(relation.subject);
+    this._verbs.add(relation.verb);
+  }
+
+  isVerb(name: string): boolean {
+    return this._verbs.has(name.toLowerCase());
+  }
+
+  isNoun(name: string): boolean {
+    return this._nouns.has(name.toLowerCase());
+  }
+
+  getVerbRelations(verb: string): FoodRelation[] {
+    return this._relations.filter((relation) => relation.verb === verb.toLowerCase());
+  }
+
+  getNounRelations(noun: string): FoodRelation[] {
+    return this._relations.filter((relation) => relation.involves(noun));
+  }
+
+  getSubjectRelations(subject: string): FoodRelation[] {
+    return this._relations.filter((relation) => relation.subject === subject.toLowerCase());
+  }
+
+  getObjectRelations(object: string): FoodRelation[] {
+    return this._relations.filter((relation) => relation.object === object.toLowerCase());
+  }
+}
+
+export class FoodRelation {
+  private _subject: string;
+  private _verb: string;
+  private _object: string;
+
+  constructor(subject: string, verb: string, object: string, private readonly originalText: string, public readonly range: qv.Range) {
+    this._subject = subject.toLowerCase();
+    this._verb = verb.toLowerCase();
+    this._object = object.toLowerCase();
+  }
+
+  get subject(): string {
+    return this._subject;
+  }
+
+  get object(): string {
+    return this._object;
+  }
+
+  get verb(): string {
+    return this._verb;
+  }
+
+  involves(noun: string): boolean {
+    const needle = noun.toLowerCase();
+    return this._subject === needle || this._object === needle;
+  }
+
+  getRangeOf(word: string): qv.Range {
+    const indexOfWord = new RegExp('\\b' + word + '\\b', 'i').exec(this.originalText)!.index;
+    return new qv.Range(this.range.start.translate({ characterDelta: indexOfWord }), this.range.start.translate({ characterDelta: indexOfWord + word.length }));
+  }
+}
